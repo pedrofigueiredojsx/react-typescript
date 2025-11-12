ddd6a06f6ee39e1740e5fd971718c1f2c8829e0a
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-12 14:00:30 -0300

    resumo vendas started

--- fintech-project/src/Components/DateRange.tsx
+++ fintech-project/src/Components/DateRange.tsx
@@ -1,24 +1,21 @@
-import React from 'react'
 import DateInput from './DateInput'
+import { useData } from '../Context/DataContext'
 
 const DateRange = () => {
-  const [inicio, setInicio] = React.useState('')
-  const [final, setFinal] = React.useState('')
+  const { inicio, setInicio, final, setFinal } = useData()
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()} className='box flex'>
       <DateInput
         label='Início'
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
-      {inicio}
       <DateInput
         label='Final'
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-      {final}
     </form>
   )
 }
--- fintech-project/src/Components/Header.tsx
+++ fintech-project/src/Components/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useData } from '../Context/DataContext'
 import DateRange from './DateRange'
+import Meses from './Meses'
 
 const Header = () => {
-
   return (
-    <div>
-      <DateRange />
-    </div>
+    <header className='mb'>
+      <div className='mb'>
+        <DateRange />
+      </div>
+      <Meses />
+    </header>
   )
 }
 
